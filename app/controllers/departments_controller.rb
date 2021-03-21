2395cf37c1222417ae32d691a5e02410b21ce52f
alex <alex@velow.com.br>
2021-03-20 23:08:01 -0300

    corrigido criação de departamentos

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -27,7 +27,7 @@ class DepartmentsController < ApplicationController
 
     respond_to do |format|
       if @department.save
-        format.html { redirect_to @department, notice: 'Department was successfully created.' }
+        format.html { redirect_to departments_path, notice: 'Department was successfully created.' }
         format.json { render :show, status: :created, location: @department }
       else
         format.html { render :new, status: :unprocessable_entity }
